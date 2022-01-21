f1f337de57bb823a2a73d7ac9dc90011e4882854
franss1234 <98148728+franss1234@users.noreply.github.com>
2022-01-21 09:34:54 +0200

    Add files via upload

---
+++ Scripts/questions.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class questions : MonoBehaviour
+{
+    public Dictionary<string, bool> database{
+      {"dog", true},
+      {"carnivour", true},
+      {"heavier", true},
+      {"movies", true},
+      {"noise", true},
+      {"fly", false},
+      {"forest", false},
+      {"ocean", false},
+      {"wash", true},
+      {"tail", true},
+      {"feathers", false},
+      {"pet", true},
+      {"claws", false},
+      {"fangs", true},
+      {"comforting", true},
+      {"desert", false},
+      {"home", true},
+      {"underwater", false},
+      {"hold", true}
+    }
+
+
+
+
+
+
+
+
+
+
+
+  /*
+
+    public List<int> animal;
+
+    public List<int> plant;
+
+    public List<int> mineral;
+
+    // If need be
+    public List<int> other;
+
+
+
+    // LIST ANSWERS
+
+    // Is it a carnivour?
+    public List<int> carnivour;
+
+    // Is it a herbivour?
+    public List<int> herbivous;
+
+    public List<int> football;
+
+    public List<int> butter;
+
+    public List<int> four_legs;
+
+    public List<int> two_legs;
+
+    public List<int> heavy;
+
+    public List<int> store;
+
+    public List<int> movies;
+
+    public List<int> noise;
+
+    public List<int> swim;
+
+    public List<int> fly;
+
+    public List<int> forest;
+
+    public List<int> ocean;
+
+    public List<int> colorful;
+
+    public List<int> big;
+
+    public List<int> wash;
+
+    public List<int> tail;
+
+    public List<int> feathers;
+
+    public List<int> pet;
+
+    public List<int> claws;
+
+    public List<int> fangs;
+
+    public List<int> dangerous;
+
+    public List<int> comforting;
+
+    public List<int> desert;
+
+    public List<int> home;
+
+    public List<int> underwater;
+
+    public List<int> hold;
+
+    public List<int> brown;
+
+
+
+
+
+
+    // ------------------------------------------- //
+
+
+    // START QUESTIONS
+
+
+    public string ask_animal = "Are you thinking of an animal?";
+
+    public string ask_plant = "Are you thinking of a plant?";
+
+    public string ask_mineral = "Are you thinking of a mineral?";
+
+
+
+
+    // QUESTIONS
+
+
+    public string ask_carnivour = "Is it a carnivour?";
+
+    // Is it a herbivour?
+    public string ask_herbivous = "Is it a h";
+
+    public string ask_football = "Is it a";
+
+    public string ask_butter = "Is it a";
+
+    public string ask_four_legs = "Is it a";
+
+    public string ask_two_legs = "Is it a";
+
+    public string ask_heavy = "Is it a";
+
+    public string ask_store = "Is it a";
+
+    public string ask_movies = "Is it a";
+
+    public string ask_noise = "Is it a";
+
+    public string ask_swim = "Is it a";
+
+    public string ask_fly = "Is it a";
+
+    public string ask_forest = "Is it a";
+
+    public string ask_ocean = "Is it a";
+
+    public string ask_colorful = "Is it a";
+
+    public string ask_big = "Is it a";
+
+    public string ask_wash = "Is it a";
+
+    public string ask_tail = "Is it a";
+
+    public string ask_feathers = "Is it a";
+
+    public string ask_pet = "Is it a";
+
+    public string ask_claws = "Is it a";
+
+    public string ask_fangs = "Is it a";
+
+    public string ask_dangerous = "Is it a";
+
+    public string ask_comforting = "Is it a";
+
+    public string ask_desert = "Is it a";
+
+    public string ask_home = "Is it a";
+
+    public string ask_underwater = "Is it a";
+
+    public string ask_hold = "Is it a";
+
+    public string ask_brown = "Is it a";
+
+
+*/
+
+
+
+
+}
---
+++ Scripts/quiz.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+
+public class quiz : MonoBehaviour
+{
+
+    public TextMeshProUGUI questionText;
+    double time = 0;
+    int counter = 0;
+
+    questions question = new questions();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        time += Time.deltaTime;
+
+        if (time > 4.0)
+        {
+            //GameObject.Find("ButtonBehaviour").GetComponent<buttonBehaviour>().questionLoop();
+            questionText.text = question.ask_animal;
+            /*if(yes()){
+              questionText.text = question.ask_carnivour;
+            }*/
+
+
+        }
+        if(counter > 0){
+          Debug.Log(counter);
+        }
+    }
+/*
+    // return something
+    int yes(int num)
+    {
+        question.animal.Add(num);
+        return num;
+    }
+*/
+
+    public void yes()
+    {
+      counter++;
+      question.
+    }
+
+
+}
